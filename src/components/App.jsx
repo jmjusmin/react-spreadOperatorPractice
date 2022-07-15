7a1bf708a19873b00044102107dbade730b4c108
jmjusmin <80433220+jmjusmin@users.noreply.github.com>
2022-07-15 16:28:18 +0700

    Finish the todo list

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [items, setItem] = useState([]); //start out with empty array
+
+  function handleChange(e) {
+    const newValue = e.target.value;
+    setInput(newValue);
+  }
+  function saveItem() {
+    setItem((prevItem) => {
+      return [...items, input];
+    });
+    setInput("");
+  }
+
+  console.log(items);
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input onChange={handleChange} type="text" value={input} />
+        <button onClick={saveItem}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item</li>
+          {items.map((item) => {
+            return <li>{item}</li>;
+          })}
         </ul>
       </div>
     </div>
